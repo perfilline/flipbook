275dc88224991ab6a33570aef44d04b7476a03c5
Gabriel de Campos <ti@perfilline.com.br>
2023-07-25 15:49:18 -0300

    removo last item from array

--- src/App.vue
+++ src/App.vue
@@ -102,14 +102,12 @@ export default {
         'images/1.png',
         'images/2.png',
         'images/3.png',
-        'images/4.png',
       ]
       this.pagesHiRes = [
         null,
         'images/1.png',
         'images/2.png',
         'images/3.png',
-        'images/4.png',
       ]
     }, 1)
 
